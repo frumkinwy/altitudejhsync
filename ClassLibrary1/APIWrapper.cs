7682aa72a3faf833f57b4399850d7ecef1ff77d5
frumkinwy <jed@virtualjacksonhole.com>
2014-05-06 23:16:21 -0600

    Primary App Design

---
+++ ClassLibrary1/APIWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifyConnector
+{
+    public class APIWrapper
+    {
+        public APIConfiguration MyAPIConfiguration { get; set; }
+        public APIWrapper(APIConfiguration api)
+        {
+            MyAPIConfiguration = api;
+        }
+
+        public void Connect()
+        {
+            
+        }
+
+        public void GetProducts()
+        {
+            
+        }
+
+    }
+    public class APIConfiguration{
+    public APIConfiguration(string apikey)
+    {
+        APIKey = apikey;
+        }
+        public string APIKey { get; set; }
+        }
+}
