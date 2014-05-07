ef8d1f06a73823dfe6f202ed03692801a4256b9f
Tim Adamson <timothy.adamson@gmail.com>
2014-05-07 22:28:49 +0200

    Added settings file

--- AlitudeApp/Program.cs
+++ AlitudeApp/Program.cs
@@ -18,12 +18,12 @@ namespace AlitudeApp
             //specify file
             string csvFile = "WEBEXPORT.xlsx";// args[0];
 
-            // this is my test store using a private api key
-            // it is yet to be tested using the public app interface as more code will need to be added
+            var settings = Properties.Settings.Default;
             var config = new PrivateApiConfiguration(
-                "https://testshop1-92.myshopify.com/admin/",
-                "73cfbcf2734ee2f0bb4693ac8ce99dbd",
-                "7d2c8f2110f9bf2e160a5cc3f5632bce");
+                settings.BaseUrl,
+                settings.ApiKey,
+                settings.Password);
+
             var api = new ApiWrapper(config);
 
             var updater = new InventoryUpdater(api);
